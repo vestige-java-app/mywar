30383cb90e49a70a2c9c16ffc99c88af9dd26454
Gaël Lalire <gael.lalire@gaellalire.fr>
2019-03-16 21:27:19 +0100

    import

---
+++ src/main/java/mypackage/Hello.java
@@ -0,0 +1,22 @@
+package mypackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+public class Hello extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("hello");
+        writer.println(IOUtils.class);
+    }
+
+}
---
+++ src/main/webapp/hi.jsp
@@ -0,0 +1,19 @@
+<HTML>
+<HEAD>
+<TITLE>Test</TITLE>
+</HEAD>
+<BODY>
+	<%!int minimum(int val1, int val2) {
+        if (val1 < val2)
+            return val1;
+        else
+            return val2;
+    }%>
+	<%
+	    int petit = minimum(Integer.parseInt(request.getParameter("max")) , 3);
+	%>
+	<p>
+		Le plus petit de 5 et 3 est
+		<%=petit%></p>
+</BODY>
+</HTML>
